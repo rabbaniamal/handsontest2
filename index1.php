857fe4e620bbbcb5da8a1c22c32d40497f7180cd
rabbaniamal <70132371+rabbaniamal@users.noreply.github.com>
2020-11-09 18:33:04 +0800

    Create index1.php

---
+++ index1.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
+
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
+"Hello Guys, I am <"'Amal Rabbani Bin Abdullah">, the Cloud Computer expert!. Please go to my GitHub account <"https://github.com/rabbaniamal/handsontest2">
